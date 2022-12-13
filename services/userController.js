5170df59e4774404efd52426703c8b81a8d988a2
AmyQuiro <amygalaxies@gmail.com>
2022-12-12 23:49:31 -0400

    metodo de confirmar

--- services/userController.js
+++ services/userController.js
@@ -44,8 +44,9 @@ async function createUser(user) {
   return { message };
 }
 
-async function add_suscription(body) {
+async function confirm_suscription(body) {
 
+  console.log('body :>> ', body);
   console.log('user_name :>> ', body.user_name);
   var susc = await suscripcionController.getSuscripcionByCode(body.suscription_code);
   console.info("====================================");
@@ -60,7 +61,7 @@ async function add_suscription(body) {
   let message = "Error in creating user";
 
   if (result.affectedRows) {
-    message = "User created successfully";
+    message = "Suscription confirm successfully.";
   }
 
   return { message };
@@ -76,7 +77,7 @@ async function login(user) {
   let message = "Error in creating user";
 
   if (result.affectedRows) {
-    message = "User created successfully";
+    message = "Authenticated";
   }
   return { message };
 }
@@ -86,6 +87,7 @@ async function login(user) {
 module.exports = {
   // getUser,
   createUser,
-  add_suscription
+  confirm_suscription,
+  login
 
 };
